ad50ebe13527ebd9b2dca7345422597ee13410c7
kebil <kebil@ronan.(none)>
2014-03-23 01:03:04 +0100

    Added the OPML functionality to calibre. But it doesn't seem to import the feeds yet :/

--- main.py
+++ main.py
@@ -13,90 +13,164 @@ if False:
     # You do not need this code in your plugins
     get_icons = get_resources = None
 
-from PyQt4.Qt import QDialog, QVBoxLayout, QPushButton, QMessageBox, QLabel
+import time
 
-#from calibre_plugins.opml.config import prefs
-#
-#class DemoDialog(QDialog):
-#
-#    def __init__(self, gui, icon, do_user_config):
-#        QDialog.__init__(self, gui)
-#        self.gui = gui
-#        self.do_user_config = do_user_config
-#
-#        # The current database shown in the GUI
-#        # db is an instance of the class LibraryDatabase2 from database.py
-#        # This class has many, many methods that allow you to do a lot of
-#        # things.
-#        self.db = gui.current_db
-#
-#        self.l = QVBoxLayout()
-#        self.setLayout(self.l)
-#
-#        self.label = QLabel(prefs['hello_world_msg'])
-#        self.l.addWidget(self.label)
-#
-#        self.setWindowTitle('OPML Importer')
-#        self.setWindowIcon(icon)
-#
-#        self.about_button = QPushButton('About', self)
-#        self.about_button.clicked.connect(self.about)
-#        self.l.addWidget(self.about_button)
-#
-#        self.conf_button = QPushButton(
-#                'Configure this plugin', self)
-#        self.conf_button.clicked.connect(self.config)
-#        self.l.addWidget(self.conf_button)
-#
-#        self.resize(self.sizeHint())
-#
-#    def about(self):
-#        # Get the about text from a file inside the plugin zip file
-#        # The get_resources function is a builtin function defined for all your
-#        # plugin code. It loads files from the plugin zip file. It returns
-#        # the bytes from the specified file.
-#        #
-#        # Note that if you are loading more than one file, for performance, you
-#        # should pass a list of names to get_resources. In this case,
-#        # get_resources will return a dictionary mapping names to bytes. Names that
-#        # are not found in the zip file will not be in the returned dictionary.
-#        text = get_resources('about.txt')
-#        QMessageBox.about(self, 'About the Interface Plugin Demo',
-#                text.decode('utf-8'))
-#
-#    def config(self):
-#        self.do_user_config(parent=self)
-#        # Apply the changes
-#        self.label.setText(prefs['hello_world_msg'])
-#
+from PyQt4.Qt import (QDialog, QGridLayout, QPushButton, QMessageBox, QLabel,
+        QWidget, QVBoxLayout, QLineEdit, QIcon, QDialogButtonBox, QTimer,
+        QScrollArea, QSize)
 
+from calibre_plugins.opml.config import prefs
+
+class Path(QWidget):
+
+    def __init__(self, parent):
+        QWidget.__init__(self, parent)
+        self.ll = ll = QGridLayout()
+        self.setLayout(self.ll)
+
+        self.labell = QLabel('Title:')
+        ll.addWidget(self.labell, 0, 0, 1, 1)
+        self.title_edit = QLineEdit(self)
+        self.title_edit.setPlaceholderText('Enter path to OPML')
+        ll.addWidget(self.title_edit, 0, 1, 1, 1)
+
+    @property
+    def title(self):
+        return unicode(self.title_edit.text())
+
+class DemoDialog(QDialog):
+
+    def __init__(self, gui, icon, do_user_config):
+        QDialog.__init__(self, gui)
+        self.gui = gui
+        self.do_user_config = do_user_config
+
+        # The current database shown in the GUI
+        # db is an instance of the class LibraryDatabase2 from database.py
+        # This class has many, many methods that allow you to do a lot of
+        # things.
+        self.db = gui.current_db
+
+        self.l = QVBoxLayout()
+        self.setLayout(self.l)
+
+        self.label = QLabel(prefs['hello_world_msg'])
+        self.l.addWidget(self.label)
+
+        self.setWindowTitle('OPML Importer')
+        self.setWindowIcon(icon)
+
+        self.import_button = QPushButton('Import', self)
+        self.import_button.clicked.connect(self.import_opml)
+        self.l.addWidget(self.import_button)
+
+        self.conf_button = QPushButton(
+                'Configure this plugin', self)
+        self.conf_button.clicked.connect(self.config)
+        self.l.addWidget(self.conf_button)
+
+        self.resize(self.sizeHint())
+
+    def import_opml(self):
+        opml = OPML();
+        opml.load('/media/sf_Kenny/Downloads/feedly.opml')
+        outlines = opml.parse()
+        opml.import_recipes(outlines)
+
+    def config(self):
+        self.do_user_config(parent=self)
+        # Apply the changes
+        self.label.setText(prefs['hello_world_msg'])
+
+
+# http://docs.python.org/2/library/xml.etree.elementtree.html
 import xml.etree.ElementTree as ET
+from calibre.web.feeds.recipes import compile_recipe, custom_recipes
+from calibre.gui2 import error_dialog
 
 class OPML(object):
 
     def __init__(self):
         self.doc = None # xml document
         self.outlines = None # parsed outline objects
+        self.oldest_article = 7
+        self.max_articles = 100
 
     def load(self, filename):
         tree = ET.parse(filename)
         self.doc = tree.getroot()
 
     def parse(self):
-        self.outlines = self.doc.findall(u'body/outline')
+        self.outlines = self.doc.findall(u"body/outline")
+
+        for outline in self.outlines: # check for groups
+            if ('type' not in outline.attrib):
+                feeds = [] # title, xmlUrl
+                for feed in outline.iter('outline'):
+                    if 'type' in feed.attrib:
+                        feeds.append( (feed.get('title'), feed.get('xmlUrl')) )
+                outline.set('xmlUrl', feeds)
         
         return self.outlines
 
-    def unload(self):
-        #self.doc.freeDoc()
-        pass
+    def import_recipes(self, outlines):
+        for outline in outlines:
+            src, title = self.options_to_profile(dict(
+                title=repr(outline.get('title')),
+                feeds=repr(outline.get('xmlUrl')),
+                oldest_article=self.oldest_article,
+                max_articles=self.max_articles,
+                base_class='AutomaticNewsRecipe'
+            ))
+            try:
+                compile_recipe(src)
+                error_dialog(None, 'Works', src).exec_()
+            except Exception as err:
+                error_dialog(None, _('Invalid input'),
+                    _('<p>Could not create recipe. Error:<br>%s')%str(err)).exec_()
+
+    def options_to_profile(self, recipe):
+        classname = 'BasicUserRecipe'+str(int(time.time()))
+        title = recipe.get('title').strip()
+        if not title:
+            title = classname
+        oldest_article = self.oldest_article
+        max_articles   = self.max_articles
+        if not isinstance(recipe.get('feeds'), str):
+            feeds = [i.user_data for i in recipe.get('feeds').items()]
+        else:
+            feeds = recipe.get('feeds')
+
+        src = '''\
+class %(classname)s(%(base_class)s):
+    title          = %(title)s
+    oldest_article = %(oldest_article)d
+    max_articles_per_feed = %(max_articles)d
+    auto_cleanup = True
+
+    feeds          = %(feeds)s
+'''%dict(classname=classname, title=repr(title),
+                 feeds=repr(feeds), oldest_article=oldest_article,
+                 max_articles=max_articles,
+                 base_class='AutomaticNewsRecipe')
+        return src, title
+
+    def add_profile(self, clicked):
+        if self.stacks.currentIndex() == 0:
+            src, title = self.options_to_profile()
+
+            try:
+                compile_recipe(src)
+            except Exception as err:
+                error_dialog(self, _('Invalid input'),
+                        _('<p>Could not create recipe. Error:<br>%s')%str(err)).exec_()
+                return
 
 if __name__ == '__main__':
-    o = OPML();
-    o.load('/media/sf_Kenny/Downloads/feedly.opml')
-    o.parse()
-    print(len(o.outlines))
-    for outline in o.outlines:
-        print(dir(outline))
-    o.unload()
+    opml = OPML();
+    opml.load('/media/sf_Kenny/Downloads/feedly.opml')
+    outlines = opml.parse()
+    print(len(opml.outlines))
+    opml.import_recipes(outlines)
+
 
